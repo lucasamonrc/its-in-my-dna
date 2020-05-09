477a92948840412bc8cfc87ddb1296e1a95d9784
lucasamonrc <lucasamonrc@gmail.com>
2020-05-08 19:34:56 -0600

    Add web server

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+
+const routes = require("./src/routes");
+
+const PORT = process.env.PORT || 3333;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+//app.use(routes);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => res.render('./views/index.html'));
+
+
+app.listen(PORT, () => `Server listening on ${PORT}`);
