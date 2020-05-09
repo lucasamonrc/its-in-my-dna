03ae79f79226f24383b3751ae302727409cd97d0
lucasamonrc <lucasamonrc@gmail.com>
2020-05-08 19:40:12 -0600

    Correct response send method for HTML

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 //app.use(routes);
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => res.send('./views/index.html'));
+app.get('/', (req, res) => res.sendFile('./views/index.html'));
 
 
 app.listen(PORT, () => `Server listening on ${PORT}`);
