9229e12075decd9e315026842d076d29750cff90
lucasamonrc <lucasamonrc@gmail.com>
2020-05-08 20:44:33 -0600

    Fix serving static files

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const express = require("express");
-const path = require("path");
 const cors = require("cors");
-
+const path = require("path");
 const routes = require("./src/routes");
 
 const PORT = process.env.PORT || 3333;
 const app = express();
-const HTML = path.join(__dirname, "/views/index.html");
 
 app.use(express.json());
 app.use(cors());
-app.use(express.static(path.join(__dirname, "public")));
 app.use(routes);
 
-app.get('/', (req, res) => res.sendFile(HTML));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
 
 
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
