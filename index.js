ba4564c66ba0c63a3d5cbb127c203445dcc0ff6c
lucasamonrc <lucasamonrc@gmail.com>
2020-05-08 20:04:28 -0600

    Uncomment app.use(routes)

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const HTML = path.join(__dirname, "/views/index.html");
 
 app.use(cors());
 app.use(express.json());
-//app.use(routes);
+app.use(routes);
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get('/', (req, res) => res.sendFile(HTML));
